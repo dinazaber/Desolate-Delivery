a9f9b1f48d4498294211823e5e449387831da84b
dinazaber <dinazaber@gmail.com>
2026-03-23 14:54:49 +0200

    Render scale setting

--- Scripts/Settings.gd
+++ Scripts/Settings.gd
@@ -2,15 +2,27 @@ extends Control
 signal closed
 
 func _on_resolution_item_selected(index: int) -> void:
+	var viewport = get_tree().root
+	viewport.scaling_3d_mode = Viewport.SCALING_3D_MODE_BILINEAR
 	match index:
 		0:
-			DisplayServer.window_set_size(Vector2i(3440,1440))
+			viewport.scaling_3d_scale = 2
 		1:
-			DisplayServer.window_set_size(Vector2i(1920,1080))
+			viewport.scaling_3d_scale = 1.75
 		2:
-			DisplayServer.window_set_size(Vector2i(1280,720))
+			viewport.scaling_3d_scale = 1.5
+		3:
+			viewport.scaling_3d_scale = 1.25
+		4:
+			viewport.scaling_3d_scale = 1
+		5:
+			viewport.scaling_3d_scale = 0.75
+		6:
+			viewport.scaling_3d_scale = 0.5
+			
 			
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("Escape"):
+	if event.is_action_pressed("Escape") and is_visible_in_tree():
+		accept_event()
 		hide()
 		closed.emit()
