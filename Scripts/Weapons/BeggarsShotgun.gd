5c184f09c13cdb4b9783ea3d55b5b29d1e9a4f88
noicemann <konstantin08.kaminski@gmail.com>
2026-04-26 15:31:02 +0300

    beggars shotgun update
    ADDITIONS:
    > pellet tracers
    > muzzle flash
    
    CHANGES:
    > now fires 9 rays (instead of 1) in a random spread within a 2.5(d) cone
    > slightly altered both shoot animations
    > damage increased (40 -> 7*9 = 63)
    
    RELATED TODOs:
    > repaint and add shader

--- Scripts/Weapons/BeggarsShotgun.gd
+++ Scripts/Weapons/BeggarsShotgun.gd
@@ -1,25 +1,50 @@
 extends Node3D
 
 #gun stats
-@export var damage: float = 40.0
+@export var damage: float = 7.0 # per pellet
 @export var recoil: float = 5.0 # degree rotation
+@export var spread: float = 2.5 # max pellet spread (degrees)
 @export var mag: int = 4
 @export var heatPerShot: float = 22.25
 @export var coolDown: float = 5.0 # time (s) it takes to go from 100 to 0 heat
 
 @export var camera: Area3D
-@export var playerRay: RayCast3D
 
 var shotNum: int = 0
 var can_cool: bool = true
 var heat: float = 0.0
 
-const MAX_DEVIATION = 5 # deviation (in degrees) of the pellets from the player's sight vector
-const MIN_INPUTS = 8
-const MAX_INPUTS = 12
-
 @onready var anim = $AnimationPlayer
 @onready var heatBuffer = $HeatBuffer
+
+@onready var pellet_1 = $BeggarsShotgun/Frame/Rays/RayCast3D1
+@onready var pellet_2 = $BeggarsShotgun/Frame/Rays/RayCast3D2
+@onready var pellet_3 = $BeggarsShotgun/Frame/Rays/RayCast3D3
+@onready var pellet_4 = $BeggarsShotgun/Frame/Rays/RayCast3D4
+@onready var pellet_5 = $BeggarsShotgun/Frame/Rays/RayCast3D5
+@onready var pellet_6 = $BeggarsShotgun/Frame/Rays/RayCast3D6
+@onready var pellet_7 = $BeggarsShotgun/Frame/Rays/RayCast3D7
+@onready var pellet_8 = $BeggarsShotgun/Frame/Rays/RayCast3D8
+@onready var pellet_9 = $BeggarsShotgun/Frame/Rays/RayCast3D9
+@onready var pellets: Array = [pellet_1,pellet_2,pellet_3,pellet_4,pellet_5,pellet_6,pellet_7,pellet_8,pellet_9]
+
+@onready var tracer_1 = $BeggarsShotgun/Frame/Rays/RayCast3D1/tracer
+@onready var tracer_2 = $BeggarsShotgun/Frame/Rays/RayCast3D2/tracer
+@onready var tracer_3 = $BeggarsShotgun/Frame/Rays/RayCast3D3/tracer
+@onready var tracer_4 = $BeggarsShotgun/Frame/Rays/RayCast3D4/tracer
+@onready var tracer_5 = $BeggarsShotgun/Frame/Rays/RayCast3D5/tracer
+@onready var tracer_6 = $BeggarsShotgun/Frame/Rays/RayCast3D6/tracer
+@onready var tracer_7 = $BeggarsShotgun/Frame/Rays/RayCast3D7/tracer
+@onready var tracer_8 = $BeggarsShotgun/Frame/Rays/RayCast3D8/tracer
+@onready var tracer_9 = $BeggarsShotgun/Frame/Rays/RayCast3D9/tracer
+@onready var tracers: Array = [tracer_1,tracer_2,tracer_3,tracer_4,tracer_5,tracer_6,tracer_7,tracer_8,tracer_9]
+@onready var flash = $BeggarsShotgun/Frame/Flash
+
+
+func _ready() -> void:
+	for pellet in pellets: # scatter
+		pellet.rotation = Vector3(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0), 0.0) * deg_to_rad(spread)
+	$BeggarsShotgun/Frame/Rays.position = camera.position
 
 func draw():
 	anim.play("draw")
@@ -45,18 +70,31 @@ func shoot():
 		if shotNum > 1: anim.play("shootConsecutive")
 		else: anim.play("shootLast")
 		
+		await scatterNshoot()
+		
 		heatBuffer.start()
 		can_cool = false
 		heat = clamp(heat + heatPerShot, 0.0, 100.0)
 		camera.add_recoil(recoil)
 		shotNum -= 1
 		
-		if playerRay.is_colliding():
-			if playerRay.get_collider().is_in_group("Enemy"):
-				playerRay.get_collider().hit(damage, "player")
-			if playerRay.get_collider().is_in_group("ShotReactable"):
-				playerRay.get_collider().shot()
 		await anim.animation_finished
+
+func scatterNshoot():
+	for pellet in pellets: # scatter
+		pellet.rotation = Vector3(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0), 0.0) * deg_to_rad(spread)
+		
+		if pellet.is_colliding(): # shoot
+			if pellet.get_collider().is_in_group("Enemy"):
+				pellet.get_collider().hit(damage, "player")
+			if pellet.get_collider().is_in_group("ShotReactable"):
+				pellet.get_collider().shot()
+	
+	for tracer in tracers: # emit
+		tracer.restart()
+		tracer.emitting = true
+	flash.restart()
+	flash.emitting = true
 
 func get_heat() -> float:
 	return heat
@@ -70,10 +108,3 @@ func _process(delta: float) -> void:
 
 func _on_heat_buffer_timeout() -> void:
 	can_cool = true
-
-func random_deviations() -> Array:
-	var deviations = []
-	var inputs = randi_range(MIN_INPUTS, MAX_INPUTS)
-	for i in range(inputs):
-		deviations.append(MAX_DEVIATION * randf())
-	return deviations
