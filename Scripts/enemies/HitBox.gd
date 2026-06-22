66342ec4443941d06e144b540040cb01af9a587b
noicemann <konstantin08.kaminski@gmail.com>
2026-06-22 23:12:23 +0300

    Revert "robot parts (don't use)"
    This reverts commit 25002a4a0216c3333b969ea66e2326f033f64eae.

--- Scripts/enemies/HitBox.gd
+++ Scripts/enemies/HitBox.gd
@@ -2,13 +2,7 @@ extends Area3D
 
 @export var isCrit: bool = false
 
-
-func _physics_process(_delta: float) -> void:
-	pass
-
 func hit(damage, isPlayer, type, pos):
-	if !owner: return
-	
 	if isCrit:
 		damage *= 1.5
 	owner.damage_taken(damage, isPlayer, type, pos)
