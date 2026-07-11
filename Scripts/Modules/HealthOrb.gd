067c687d3ecc93164e239cb40bef60c8558f80b1
dinazaber <dinazaber@gmail.com>
2026-07-11 22:24:08 +0300

    Small health orbs additions + better glow effect
    *Health orbs now heal and disappear only if player_health < 100. Otherwise, they remain in the air.
    Related idea: add a timer that will remove the orb if it wasn't used x time.
    * Added better glow effect that is close to Forward+ glow rendering, requires setup.
    Disadvantages:
    Doesn't work very well with particles.
    More draw calls :(
    
    I will try to somehow change the shader so it won't require duplicating geometry and will support particles as well.

--- Scripts/Modules/HealthOrb.gd
+++ Scripts/Modules/HealthOrb.gd
@@ -17,14 +17,16 @@ func _on_gravitate_range_body_entered(body: Node3D) -> void:
 
 func _on_pickup_range_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
-		body.heal(heal_amount)
-		queue_free()
+		if body.player_health < 100:
+			body.heal(heal_amount)
+			queue_free()
 
 func _physics_process(_delta: float) -> void:
 	linear_damp = clamp(linear_velocity.length(), 4.0, 8.0)
 	
 	if player:
-		var dir: Vector3 = player.global_position - Vector3(0.0,0.25,0.0) - global_position
-		apply_central_force(dir * clamp(dir.length(), 1.5, 2.0) * clamp(linear_velocity.length(), 0.5, 4.0))
+		if player.player_health < 100 and player.global_position.distance_to(global_position) < 3.5:
+			var dir: Vector3 = player.global_position - Vector3(0.0,0.25,0.0) - global_position
+			apply_central_force(dir * clamp(dir.length(), 1.5, 2.0) * clamp(linear_velocity.length(), 0.5, 4.0))
 	elif !fallCheck.is_colliding():
 		apply_central_force(Vector3.DOWN * clamp(linear_velocity.length(), 0.5, 1.5))
